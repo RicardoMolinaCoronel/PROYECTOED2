673fa18a0ced469d44d91b46893861b44687903c
carana08 <carana@espol.edu.ec>
2023-01-12 17:04:46 -0500

    Actualizando

--- src/ed2g9/ED2G9.java
+++ src/ed2g9/ED2G9.java
@@ -5,12 +5,18 @@
  */
 package ed2g9;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -18,6 +24,9 @@ import javafx.stage.Stage;
  * @author Ricardo
  */
 public class ED2G9 extends Application {
+    private String[] colores = {"#55efc4", "#81ecec", "#74b9ff", "#74b9ff", "#a29bfe", "#dfe6e9", "#00b894", "#00cec9", "#0984e3", "#6c5ce7", "#b2bec3", "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8",
+         "#636e72", "#fdcb6e", "#e17055", "#d63031", "#e84393"};
+    private int contador=1;
     
     @Override
     public void start(Stage primaryStage) {
@@ -31,6 +40,7 @@ public class ED2G9 extends Application {
             }
         });
         
+        
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         
@@ -39,6 +49,34 @@ public class ED2G9 extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    public void PanelPrincipal(){
+        Stage stage= new Stage();
+        Label lb= new Label("Elija la carpeta a representar");
+        lb.setTextFill(Color.web("#3498db"));
+        lb.setFont(new Font("Arial Black",18));
+        Button bt= new Button("Navegar");
+        bt.setStyle("-fx-font-size: 25px; -fx-font-weight: bold; -fx-padding: 20px 25px;-fx-background-color: #9b59b6;"/*+"-fx-border-color: #ED4C67"*/);
+        bt.setTextFill(Color.web("#ffffff"));
+        
+        //aviso.setTextFill(Color.web("#e74c3c"));
+        Label aviso= new Label();
+        aviso.setTextFill(Color.web("#e74c3c"));
+        aviso.setFont(new Font("Arial Black",18));
+        aviso.setVisible(false);
+        VBox vb= new VBox(lb,aviso,bt);
+        vb.setAlignment(Pos.CENTER);
+        vb.setSpacing(40);
+        vb.setStyle("-fx-background-color: #bdc3c7;");
+        bt.setOnAction(new EventHandler<ActionEvent>(){
+             @Override
+             public void handle(ActionEvent event) {
+                 //falta la parde del handel de agregar
+             }
+         });
+        Scene scene= new Scene(vb,600,300);
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
